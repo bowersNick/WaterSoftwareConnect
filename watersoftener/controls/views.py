99972c863640ca48afc805615f92ebbe05386d24
Nick Bowers <nickbowers999@gmail.com>
2018-12-27 10:24:01 -0600

    Changed stylesheet reference to use {% static "" %}.

--- watersoftener/controls/views.py
+++ watersoftener/controls/views.py
@@ -1,22 +1,22 @@
-import arrow
 from django.shortcuts import render
-
 # Create your views here.
-from django.views import View
 # from controls.forms import ControlsForm
+from django.views.generic import DetailView
 
-class ControlsView(View):
-    # form_class = ControlsForm
-    # initial = {'key': 'value'}
+from .models import ControlsModel
+
+
+class ControlsView(DetailView):
+    model = ControlsModel
     template_name = 'controls/index.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name,
-                      {"salt_level": 0.5, "vacation_mode": "Off", "days_away": 2, "system_alarm": "Off",
-                       "system_alarm_time": arrow.now().strftime("%H:%M:%S %m-%d-%Y"),
-                       "time_of_day": arrow.now().format("h:mm:ss a"), "current_day_of_week": arrow.now().format("dddd"),
-                       "current_date": arrow.now().format("M/D/YYYY"), "auto_dst": "Off", })
-
-    def post(self, request, *args, **kwargs):
-        print(request)
         return render(request, self.template_name)
+                      # {"salt_level": 0.5, "vacation_mode": "Off", "days_away": 2, "system_alarm": "Off",
+                      #  "system_alarm_time": arrow.now().strftime("%H:%M:%S %m-%d-%Y"),
+                      #  "time_of_day": arrow.now().format("h:mm:ss a"), "current_day_of_week": arrow.now().format("dddd"),
+                      #  "current_date": arrow.now().format("M/D/YYYY"), "auto_dst": "Off", })
+
+    # def post(self, request, *args, **kwargs):
+    #     self.object = self.get_object()
+    #     return super().post(request, *args, **kwargs)
